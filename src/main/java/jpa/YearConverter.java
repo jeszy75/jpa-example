06fac22453ce3f418b5a331d1f0b2c6ba16bf1ec
Péter Jeszenszky <jeszenszky.peter.deik@gmail.com>
2020-04-20 13:05:14 +0200

    Added null handling to YearConverter

--- src/main/java/jpa/YearConverter.java
+++ src/main/java/jpa/YearConverter.java
@@ -10,12 +10,12 @@ public class YearConverter implements AttributeConverter<Year, Integer> {
 
     @Override
     public Integer convertToDatabaseColumn(Year year) {
-        return year.getValue();
+        return year != null ? year.getValue() : null;
     }
 
     @Override
     public Year convertToEntityAttribute(Integer value) {
-        return Year.of(value);
+        return value != null ? Year.of(value) : null;
     }
 
 }
